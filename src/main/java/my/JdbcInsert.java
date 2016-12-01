b8a86e40b18eff0b897b0b418d0382231c5e90f2
Peng.Li <nowind_lee@qq.com>
2016-12-01 09:56:02 +0800

    get db generated keys

--- src/main/java/my/JdbcInsert.java
+++ src/main/java/my/JdbcInsert.java
@@ -14,10 +14,14 @@ public class JdbcInsert {
         books.put("JavaScript", "另一本好书");
         for (String title : books.keySet()) {
             String description = books.get(title);
-            PreparedStatement stmt = conn.prepareStatement("insert into books(title, description) values(?, ?)");
+            PreparedStatement stmt = conn.prepareStatement("insert into books(title, description) values(?, ?)", Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, title);
             stmt.setString(2, description);
-            stmt.execute();
+            stmt.executeUpdate();
+            ResultSet generatedKeys = stmt.getGeneratedKeys();
+            generatedKeys.next();
+            System.out.println("id created by database is: " + generatedKeys.getLong(1));
+            generatedKeys.close();
             stmt.close();
         }
         conn.close();
